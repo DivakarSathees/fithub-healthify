b25a674848a9f916dd33373d2837f4e0b2c560a1
Divakar <divakarsathees7@gmail.com>
2023-02-17 15:05:50 +0530

    href to Link

--- src/pages/Workuser.js
+++ src/pages/Workuser.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { StyledFormButton, StyledButton, ButtonGroup} from '../components/Styles'
 
 const Workuser = ({ logoutUser,user }) => {
@@ -9,10 +10,10 @@ const Workuser = ({ logoutUser,user }) => {
   return (
     <div>
         {/* <ButtonGroup> */}
-        <a href={`/Workout/${user.name}/${user._id}`} style={{textDecoration:'none'}}><ButtonGroup><StyledFormButton to={`/Workout/${user.name}/${user._id}`} style={{width: '300px'}}>Enroll For Workouts</StyledFormButton></ButtonGroup></a>
+        <Link to={`/Workout/${user.name}/${user._id}`} style={{textDecoration:'none'}}><ButtonGroup><StyledFormButton to={`/Workout/${user.name}/${user._id}`} style={{width: '300px'}}>Enroll For Workouts</StyledFormButton></ButtonGroup></Link>
         {/* </ButtonGroup> */}
-        <a href={`/EditProfile/${user._id}`} style={{textDecoration:'none'}}><ButtonGroup><StyledFormButton to={'/Home'} style={{width: '300px'}}>Edit Profile</StyledFormButton></ButtonGroup></a>
-        <a href={`/Schedule/${user._id}`}style={{textDecoration:'none'}}><ButtonGroup><StyledFormButton to={'/Home'} style={{width: '300px'}}>View Workouts</StyledFormButton></ButtonGroup></a>
+        <Link to={`/EditProfile/${user._id}`} style={{textDecoration:'none'}}><ButtonGroup><StyledFormButton to={'/Home'} style={{width: '300px'}}>Edit Profile</StyledFormButton></ButtonGroup></Link>
+        <Link to={`/Schedule/${user._id}`}style={{textDecoration:'none'}}><ButtonGroup><StyledFormButton to={'/Home'} style={{width: '300px'}}>View Workouts</StyledFormButton></ButtonGroup></Link>
         
         <div>
             &nbsp;
